5838c57aaa1fe54a7afccb8b61403bdbb50e82bf
runhannah <17864983+RunHannah@users.noreply.github.com>
2025-02-16 21:39:19 -0800

    pagination bar

---
+++ components/PaginationBar.tsx
@@ -0,0 +1,77 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { PAGINATION } from "@/app/lib/constants";
+
+interface PaginationBarProps {
+  currentPage: number;
+  totalPages: number;
+  handlePagination: (action: string) => void;
+}
+
+export default function PaginationBar({
+  currentPage,
+  totalPages,
+  handlePagination,
+}: PaginationBarProps) {
+  return (
+    <Pagination>
+      <PaginationContent>
+        
+        {/* Previous Button */}
+        <PaginationItem>
+          <PaginationPrevious
+            className={
+              currentPage === 1
+                ? "cursor-not-allowed opacity-50"
+                : "cursor-pointer"
+            }
+            onClick={() => handlePagination(PAGINATION.PREVIOUS_PAGE)}
+          />
+        </PaginationItem>
+
+        {/* Current Page */}
+        <PaginationItem className="cursor-not-allowed opacity-50">
+          {currentPage}
+        </PaginationItem>
+
+        {/* Ellipsis */}
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+
+        {/* Last Page */}
+        <PaginationItem>
+          <PaginationLink
+            className={
+              currentPage === totalPages
+                ? "cursor-not-allowed opacity-50"
+                : "cursor-pointer"
+            }
+            onClick={() => handlePagination(PAGINATION.LAST_PAGE)}
+          >
+            {totalPages}
+          </PaginationLink>
+        </PaginationItem>
+
+        {/* Next Button */}
+        <PaginationItem>
+          <PaginationNext
+            className={
+              currentPage === totalPages
+                ? "cursor-not-allowed opacity-50"
+                : "cursor-pointer"
+            }
+            onClick={() => handlePagination(PAGINATION.NEXT_PAGE)}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
