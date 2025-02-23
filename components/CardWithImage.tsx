89a232c9e7c6607b0a844d659ec7fd0d8daaed0b
runhannah <17864983+RunHannah@users.noreply.github.com>
2025-02-23 15:40:54 -0800

    hover container

--- components/CardWithImage.tsx
+++ components/CardWithImage.tsx
@@ -22,7 +22,7 @@ export default function CardWithImage({
   ...props
 }: CardWithImageProps) {
   return (
-    <Card className="max-w-[200px] mt-2 mb-2 flex flex-col h-fit rounded-none">
+    <Card className="max-w-[200px] mt-2 mb-2 flex flex-col h-fit rounded-none relative overflow-hidden">
       <Image
         src={src}
         alt={alt}
@@ -33,13 +33,20 @@ export default function CardWithImage({
         blurDataURL={"/placeholder.svg?height=50&width=50"}
         {...props}
       />
-      <CardHeader className="p-3">
-        <CardTitle>{title || "Unknown"}</CardTitle>
-      </CardHeader>
-      <CardContent className="pl-3 pb-3 pr-3 text-xs">
-        <p>Year: {year || "Unknown"}</p>
-        <p>Country: {country || "Unknown"}</p>
-      </CardContent>
+
+      {/* Hover effect container */}
+      <div className="absolute inset-0 text-white bg-black bg-opacity-0 hover:bg-opacity-70 transition-all duration-300">
+        {/* Text is hidden by default and appears on hover */}
+        <div className="w-full h-full flex flex-col justify-center items-center text-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+          <CardHeader className="p-3">
+            <CardTitle>{title || "Unknown"}</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-3 pb-3 pr-3 text-xs">
+            <p>Year: {year || "Unknown"}</p>
+            <p>Country: {country || "Unknown"}</p>
+          </CardContent>
+        </div>
+      </div>
     </Card>
   );
 }
