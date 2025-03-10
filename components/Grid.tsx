c7bfc29e1b9a01b972bee67eedbcc16a0741e42e
runhannah <17864983+RunHannah@users.noreply.github.com>
2025-03-09 21:54:07 -0700

    animate search results

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,11 +1,33 @@
+import { motion } from "motion/react";
+import { Skeleton } from "@/components/ui/skeleton";
+
 interface GridProps {
+  isLoading: boolean;
   children: React.ReactNode;
 }
 
-export default function Grid({ children }: GridProps) {
+export default function Grid({ isLoading, children }: GridProps) {
+  const gridVariants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+        delayChildren: 0.5,
+      },
+    },
+  };
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center">
-      {children}
-    </div>
+    <motion.div
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center"
+      variants={gridVariants}
+      initial="hidden"
+      animate="show"
+    >
+      {isLoading
+        ? Array.from({ length: 25 }).map((_, index) => <Skeleton key={index} />)
+        : children}
+    </motion.div>
   );
 }
